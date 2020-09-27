7f883b4862d7008f313b41bc3ed0b6e8f506c2e4
Lane Harvey <mrlaneharvey@gmail.com>
2020-09-26 19:55:48 -0600

    added weather api integrations

--- main.js
+++ main.js
@@ -3,3 +3,24 @@ const time = (today.getHours() % 12) + ":" + (today.getMinutes()<10?'0':'') + to
 
 const clock = document.querySelector(".clock");
 clock.innerHTML = time;
+
+let currentCondition;
+let currentTemp;
+let currentTempIcon
+
+const url = 'http://api.weatherapi.com/v1/current.json?key=0a3fb8be05f748b99d5223448202609&q=Ogden';
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    currentCondition = data.current.condition.text;
+    currentTemp = data.current.temp_f;
+    currentConditionIcon = data.current.condition.icon;
+    const weather = document.querySelector(".current-weather");
+    weather.innerHTML = `${currentCondition}`;
+    const temp = document.querySelector(".current-temp");
+    temp.innerHTML = `
+        <img src="${currentConditionIcon}"></img>
+        ${currentTemp}°
+    `;
+});
